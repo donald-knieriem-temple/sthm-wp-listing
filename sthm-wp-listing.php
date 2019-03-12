6e6189ac0dd8239adc94a21bd15404dac32d691f
Donald Knieriem <donald.knieriem@temple.edu>
2019-03-12 10:05:16 -0400

    init

---
+++ sthm-wp-listing.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: STHM Job Listings
+Description: Custom post type for STHM externship, internship, and assistantship listings
+*/
+/* Start Adding Functions Below this Line */
+
+  
+  
+/* Stop Adding Functions Below this Line */
+?>
